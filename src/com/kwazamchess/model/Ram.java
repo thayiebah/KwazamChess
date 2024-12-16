f29b75a27a1f1e600a3321562978e0f0f4596447
Thayiebah <thayiebah2003@gmail.com>
2024-12-17 00:05:09 +0800

    ram

--- src/com/kwazamchess/model/Ram.java
+++ src/com/kwazamchess/model/Ram.java
@@ -1,5 +1,17 @@
 package com.kwazamchess.model;
 
-public class Ram {
-    
+public class Ram extends ChessPiece {
+
+    public Ram(Position position, String colour) {
+        super(position, colour);
+    }
+
+    @Override
+    public boolean isValidMove(Position target) {
+        // Ram can only move forward one step
+        int direction = colour.equalsIgnoreCase("Blue") ? 1 : -1;
+
+        // Check if the target is exactly one step forward
+        return target.getX() == position.getX() + direction && target.getY() == position.getY();
+    }
 }
